afc3b9218cc59e5234337133ae140d6b708c2cff
DTennant <Tennant_1999@outlook.com>
2018-11-03 14:49:23 +0800

    first commit

---
+++ main_half.py
@@ -0,0 +1,145 @@
+# coding:utf8
+
+import torch as t
+import torchvision as tv
+import torchnet as tnt
+
+from torch.utils import data
+from transformer_net import Transformer
+import utils
+from PackedVGG import Vgg16
+from torch.nn import functional as F
+import tqdm
+import os
+import ipdb
+
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+
+class Config(object):
+    image_size = 64  # 图片大小
+    batch_size = 256
+    data_root = 'data/'  # 数据集存放路径：data/coco/a.jpg
+    num_workers = 4  # 多线程加载数据
+    use_gpu = True  # 使用GPU
+
+    style_path = 'style.jpg'  # 风格图片存放路径
+    lr = 1e-3  # 学习率
+
+    env = 'neural-style-small'  # visdom env
+    plot_every = 10  # 每10个batch可视化一次
+
+    epoches = 1000  # 训练epoch
+
+    content_weight = 1e5  # content_loss 的权重
+    style_weight = 1e10  # style_loss的权重
+
+    model_path = None  # 预训练模型的路径
+    debug_file = 'tmp/debugnn'  # touch $debug_fie 进入调试模式
+
+    content_path = 'input.png'  # 需要进行分割迁移的图片
+    result_path = 'output.png'  # 风格迁移结果的保存路径
+
+
+def train(**kwargs):
+    opt = Config()
+    for k_, v_ in kwargs.items():
+        setattr(opt, k_, v_)
+    
+    device=t.device('cuda') if opt.use_gpu else t.device('cpu')
+    vis = utils.Visualizer(opt.env)
+
+    # 数据加载
+    transfroms = tv.transforms.Compose([
+        tv.transforms.Resize(opt.image_size),
+        tv.transforms.CenterCrop(opt.image_size),
+        tv.transforms.ToTensor(),
+        tv.transforms.Lambda(lambda x: x * 255)
+    ])
+    dataset = tv.datasets.ImageFolder(opt.data_root, transfroms)
+    dataloader = data.DataLoader(dataset, opt.batch_size)
+
+    # 转换网络
+    transformer = Transformer()
+    if opt.model_path:
+        transformer.load_state_dict(t.load(opt.model_path, map_location=lambda _s, _: _s))
+    transformer.to(device)
+
+    # 损失网络 Vgg16
+    vgg = Vgg16().eval()
+    vgg.to(device)
+    for param in vgg.parameters():
+        param.requires_grad = False
+
+    # 优化器
+    optimizer = t.optim.Adam(transformer.parameters(), opt.lr)
+
+    # 获取风格图片的数据
+    style = utils.get_style_data(opt.style_path)
+    vis.img('style', (style.data[0] * 0.225 + 0.45).clamp(min=0, max=1))
+    style = style.to(device)
+
+
+    # 风格图片的gram矩阵
+    with t.no_grad():
+        features_style = vgg(style)
+        gram_style = [utils.gram_matrix(y) for y in features_style]
+
+    # 损失统计
+    style_meter = tnt.meter.AverageValueMeter()
+    content_meter = tnt.meter.AverageValueMeter()
+
+    for epoch in range(opt.epoches):
+        content_meter.reset()
+        style_meter.reset()
+
+        for ii, (x, _) in tqdm.tqdm(enumerate(dataloader)):
+
+            # 训练
+            optimizer.zero_grad()
+            x = x.to(device)
+            y = transformer(x)
+            y = utils.normalize_batch(y)
+            x = utils.normalize_batch(x)
+            features_y = vgg(y)
+            features_x = vgg(x)
+
+            # content loss
+            content_loss = opt.content_weight * F.mse_loss(features_y.relu2_2, features_x.relu2_2)
+
+            # style loss
+            style_loss = 0.
+            for ft_y, gm_s in zip(features_y, gram_style):
+                gram_y = utils.gram_matrix(ft_y)
+                style_loss += F.mse_loss(gram_y, gm_s.expand_as(gram_y))
+            style_loss *= opt.style_weight
+
+            total_loss = content_loss + style_loss
+            total_loss.backward()
+            optimizer.step()
+
+            # 损失平滑
+            content_meter.add(content_loss.item())
+            style_meter.add(style_loss.item())
+
+            if (ii + 1) % opt.plot_every == 0:
+                if os.path.exists(opt.debug_file):
+                    ipdb.set_trace()
+
+                # 可视化
+                vis.plot('content_loss', content_meter.value()[0])
+                vis.plot('style_loss', style_meter.value()[0])
+                # 因为x和y经过标准化处理(utils.normalize_batch)，所以需要将它们还原
+                vis.img('output', (y.data.cpu()[0] * 0.225 + 0.45).clamp(min=0, max=1))
+                vis.img('input', (x.data.cpu()[0] * 0.225 + 0.45).clamp(min=0, max=1))
+
+        # 保存visdom和模型
+        vis.save([opt.env])
+        t.save(transformer.state_dict(), 'checkpoints/%s_style.pth' % epoch)
+
+
+if __name__ == '__main__':
+    import fire
+
+    fire.Fire()
---
+++ transformer_net.py
@@ -0,0 +1,45 @@
+# coding:utf8
+
+import torch as t
+from torch import nn
+import numpy as np
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(32, 64, 1)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(64, 32, 1)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.conv4 = nn.Conv2d(32, 3, 3, padding=1)
+        self.relu4 = nn.ReLU(inplace=True)
+        
+        self.conv5 = nn.Conv2d(3, 32, 3, padding=1)
+        self.relu5 = nn.ReLU(inplace=True)
+        self.conv6 = nn.Conv2d(32, 64, 1)
+        self.relu6 = nn.ReLU(inplace=True)
+        self.conv7 = nn.Conv2d(64, 32, 1)
+        self.relu7 = nn.ReLU(inplace=True)
+        self.conv8 = nn.Conv2d(32, 3, 3, padding=1)
+    
+    def forward(self, x):
+        t = self.conv1(x)
+        t = self.relu1(t)
+        t = self.conv2(t)
+        t = self.relu2(t)
+        t = self.conv3(t)
+        t = self.relu3(t)
+        t = self.conv4(t)
+        t = self.relu4(t) + x
+        
+        t = self.conv5(t)
+        t = self.relu5(t)
+        t = self.conv6(t)
+        t = self.relu6(t)
+        t = self.conv7(t)
+        t = self.relu7(t)
+        t = self.conv8(t) + x
+        return t
